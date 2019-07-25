3055fa2132e6f5cbed8c6c6a15bb7d070b532c9c
ruthless96 <halonahaley@naver.com>
2019-07-25 09:37:08 +0900

    delet
    e

--- Outflearn/src/main/java/com/outflearn/Outflearn/model/biz/LoginBiz.java
+++
@@ -1,9 +0,0 @@
-package com.outflearn.Outflearn.model.biz;
-
-import com.outflearn.Outflearn.dto.UserInfoDto;
-
-public interface LoginBiz {
-	
-	public UserInfoDto loginDo(String id, String pw);
-
-}
--- Outflearn/src/main/webapp/resources/js/template/detail.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.nav-tabs > li > a').on('click', function() {
-        $(this).css({ 'border-bottom': '2px solid #6473ff'});
-        $(this).parents().siblings().children().css({ 'border-bottom': ''});
-    })
-})
